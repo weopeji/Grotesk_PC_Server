a672345240d056948916854cc74ecf574d02c9d5
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2020-11-13 13:26:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// Lol nice work!
 const express                       = require('express');
 const mongoose                      = require('mongoose');
 const bodyParser                    = require('body-parser');
