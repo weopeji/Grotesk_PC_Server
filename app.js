344d38418f878887aa816bdcc475231e7a552613
Kirill Maximov <52042792+weopeji@users.noreply.github.com>
2020-11-13 05:50:25 +0300

    Update app.js

--- app.js
+++ app.js
@@ -44,11 +44,11 @@ app.post('/git', function (req, res) {
     req.on('data', function(chunk) {
         let sig = "sha1=" + crypto.createHmac('sha1', secret).update(chunk.toString()).digest('hex');
 
-        if (req.headers['x-hub-signature'] == sig) {
+     
 		exec('cd ' + repo + ' && git pull https://github.com/weopeji/Grotesk_PC_Server.git', function(err, stdout, stderr) {
         		console.log(stdout);
 		})
-        }
+        
     });
 
     res.status(200);
